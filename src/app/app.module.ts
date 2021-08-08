511d3738a6d9cfd0f1d547a4fe472fb6ae724a03
nshimiyimano-cloud <njeanluc828@gmail.com>
2021-08-08 22:30:00 +0200

    all compnt needed&routinsetup endwith its urlpaths

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,61 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SigninComponent } from './signin/signin.component';
+import { SignupComponent } from './signup/signup.component';
+import { RequestResetComponent } from './request-reset/request-reset.component';
+import { ResponseResetComponent } from './response-reset/response-reset.component';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+{
+  path:'sign-in',
+  component:SigninComponent
+},
+
+{
+  path:'sign-up',
+  component:SignupComponent
+},
+
+{
+  path:'request-reset-password',
+  component:RequestResetComponent
+},
+
+{
+  path:'response-reset-password',
+  component:ResponseResetComponent
+},
+
+
+{
+  path:'response-reset-password',
+  redirectTo:'sign-in'
+},
+
+
+
+];
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SigninComponent,
+    SignupComponent,
+    RequestResetComponent,
+    ResponseResetComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
-  ],
+    AppRoutingModule,
+    RouterModule.forRoot(routes),
+    ],
+
+  exports:[RouterModule],
+
+
   providers: [],
   bootstrap: [AppComponent]
 })
